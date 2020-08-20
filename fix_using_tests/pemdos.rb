00fa0785b9f792d67bce4f02bb4701c8f2925003
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-20 21:36:00 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,8 +3,7 @@ require "pry"
 def snake_it_up(string)
   #binding.pry
   if string[0] == "s"
-    snake = ("s" * 10) + string
-    puts snake 
+    puts ("s" * 10) + string
   else
       string
   end
