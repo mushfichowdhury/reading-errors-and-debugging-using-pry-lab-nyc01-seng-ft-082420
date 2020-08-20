bcb6a4a9c2b2ff9602c75dc348d05d12a3361674
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-20 21:35:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,12 +1,12 @@
 # don't forget to add: require 'pry'
 require "pry" 
 def snake_it_up("string")
-  binding.pry
+  #binding.pry
   if string[0] == "s"
     snake = ("s" * 10) + string
     puts snake 
   else
       string
   end
-  binding.pry
+  #binding.pry
 end
